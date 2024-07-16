026783a8859c092defc579a02995c773e273ff32
James-wasonga <jameswasonga22@gmail.com>
2024-07-16 10:31:27 +0300

    fetching real time weather data from api

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const timeEl = document.getElementById('time');
 const dateEl = document.getElementById('date');
-const currentWeatherItemEl = document.getElementById('current-weather-items');
+const currentWeatherItemsEl = document.getElementById('current-weather-items');
 const timezone = document.getElementById("time-zone");
 const countryEl = document.getElementById('country');
 const weatherForecastEl = document.getElementById('weather-forecast')
@@ -8,6 +8,11 @@ const currentTempEl = document.getElementById('current-temp');
 
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul', 'Aug','Sep','Oct','Nov', 'Dec'];
+
+// const API_KEY = 'c29ac16ed3438cf9497a709d6cb07f0e';
+const API_KEY = '49cc8c821cd2aff9af04c9f98c36eb74';
+
+
 setInterval(() => {
     const time = new Date();
     // console.log(time);
@@ -24,5 +29,48 @@ setInterval(() => {
  
 }, 1000);
 
+
+function getWeatherData(){
+    navigator.geolocation.getCurrentPosition(success => {
+        // console.log(success);
+         let {latitude, longitude } = success.coords;
+        
+         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`)
+         .then(response => response.json())
+         .then(data => {
+            console.log(data);
+            showWeatherData(data)
+         })
+    })
+}
+getWeatherData();
+
+
+function showWeatherData(data){
+    let [humidity, pressure, sunrise, sunset, wind_speed ] = data.current;
+
+    currentWeatherItemsEl.innerHTML = 
+
+    ` <div class="weather-item">
+    <div>Humidity</div>
+    <div>${humidity}</div> 
+</div>
+<div class="weather-item">
+    <div>Pressure</div>
+    <div>${pressure}</div> 
+</div> <div class="weather-item">
+    <div>Wind Speed</div>
+    <div>${wind_speed}</div> 
+</div>
+</div> <div class="weather-item">
+    <div>Sunrise</div>
+    <div>${window.moment(sunrise * 1000).format('HH:mm a')}</div> 
+</div>
+</div> <div class="weather-item">
+    <div>Sunset</div>
+    <div>${window.moment(sunset * 1000).format('HH:mm a')}</div> 
+</div>`;
+
+}
 
  
